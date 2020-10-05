77df12fea67b6e75042a07e8c39cdef805465f9d
christopherdonohue <christopherdonohue827@gmail.com>
2020-10-05 16:21:25 -0400

    test

--- server.js
+++ server.js
@@ -3,19 +3,20 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use(cors());
-
-
+app.use('/', express.static('public'));
 
 var fs = require('fs');
 const budget = JSON.parse(fs.readFileSync('newFile.json', 'utf8'));
 
 
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
+});
 
 app.get('/budget', (req, res) => {
     res.json(budget);
 });
 
 app.listen(port, () => {
-console.log(`API served at http://localhost:${port}`) 
+console.log(`Example app listening at http://localhost:${port}`) 
 });
